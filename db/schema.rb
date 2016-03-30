fcf7ccd9470bde37b8674a5d02ac31138a3c9c64
Paige <pnkrsnak@gmail.com>
2016-03-30 12:57:20 -0400

    updated db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160329184839) do
+ActiveRecord::Schema.define(version: 20160330155522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ability_scores", force: :cascade do |t|
+    t.integer  "strength"
+    t.integer  "dexterity"
+    t.integer  "constitution"
+    t.integer  "intelligence"
+    t.integer  "wisdom"
+    t.integer  "charisma"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "magic_items", force: :cascade do |t|
     t.string   "name"
