479ef404787dc0e1417df277deaf688e0cdcb5c5
Paige <pnkrsnak@gmail.com>
2018-05-21 13:52:55 -0400

    pg db conversion

--- andromede/settings.py
+++ andromede/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'andromede.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'androdb',
+		'USER': 'androdbuser',
+		'PASSWORD': 'h0wNOWt4nmoo',
+		'HOST': 'localhost',
+		'PORT': '',
     }
 }
 
