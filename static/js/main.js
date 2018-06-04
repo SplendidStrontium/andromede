b022f0dc2cea38e1efa04f287f7ddb988946867d
Paige N. Krsnak <pnkrsnak@gmail.com>
2018-06-04 02:59:29 +0000

    canvas logo scales

--- static/js/main.js
+++ static/js/main.js
@@ -1,30 +1,93 @@
 // main.js 
+$(document).ready(function() {
+    //alert(document.getElementById("base-nav").offsetHeight);
 
-// canvas logic
-function draw() {
-    var canvas = document.getElementById('tlogo');
+    var logoCanvas = document.getElementById('tlogo');
 
-    if (canvas.getContext) {
-        var ctx = canvas.getContext('2d');
-       
-        ctx.font = '58px Titillium Web';
-        ctx.fillStyle = '#68dfed';
-        ctx.fillText('androme.de', 60,67); 
+    if (logoCanvas.getContext) {
+        var ctx = logoCanvas.getContext('2d');
 
-        ctx.beginPath();
-        ctx.moveTo(138,22);
-        ctx.bezierCurveTo(34,40,10,80,120,80);
-        ctx.strokeStyle = '#58a64c';
-        ctx.stroke();
+        initialize();
 
-        ctx.beginPath();
-        ctx.ellipse(120,74,10,6,0,0,2 * Math.PI);
-        ctx.stroke();
+        function initialize() {
+            window.addEventListener('resize', resizeCanvas, false);
 
-    } else {
-        // canvas-unsupported code here
+            resizeCanvas();
+        }
+
+        function redraw() {
+            ctx.font = getFont();
+
+            function getFont() {
+                // default font is 119 for 700 px wide canvas
+                var ratio = 119 / 700;
+                var size = logoCanvas.width * ratio;
+                return (size|0) + 'px Titillium Web';
+            }
+            
+
+            ctx.fillStyle = '#68dfed';
+            var lpad = logoCanvas.width * 0.08;
+            var tpad = logoCanvas.height * 0.71;
+            ctx.fillText('androme.de', lpad,tpad);
+
+            ctx.beginPath();
+            var pt1x = logoCanvas.width * 0.25;
+            var pt1y = logoCanvas.height * 0.04;
+            ctx.moveTo(pt1x,pt1y);
+            var c1x = logoCanvas.width * 0.02;
+            var c1y = logoCanvas.height * 0.43;
+            var c2x = logoCanvas.width * 0.02;
+            var c2y = logoCanvas.height * 0.88;
+            var pt2x = logoCanvas.width * 0.19;
+            var pt2y = logoCanvas.height * 0.88;
+            ctx.bezierCurveTo(c1x,c1y,c2x,c2y,pt2x,pt2y);
+            ctx.strokeStyle = '#58a64c';
+            ctx.stroke();
+
+            var ewid = logoCanvas.width * 0.014;
+            var ehei = logoCanvas.height * 0.029;
+
+            ctx.beginPath();
+            var s1x = logoCanvas.width * 0.19;
+            var s1y = logoCanvas.height * 0.88;
+            ctx.ellipse(s1x,s1y,ewid,ehei,0,0,2*Math.PI);
+            ctx.fillStyle = '#58a64c';
+            ctx.fill();
+
+            ctx.beginPath();
+            var s2x = s1x + (logoCanvas.width * 0.1);
+            ctx.ellipse(s2x,s1y,ewid,ehei,0,0,2*Math.PI);
+            ctx.fillStyle = '#58a64c';
+            ctx.fill();
+
+            ctx.beginPath();
+            var s3x = s2x + (logoCanvas.width * 0.2);
+            ctx.ellipse(s3x,s1y,ewid,ehei,0,0,2*Math.PI);
+            ctx.fillStyle = '#58a64c';
+            ctx.fill();
+
+            ctx.beginPath();
+            var s4x = s3x + (logoCanvas.width * 0.3);
+            ctx.ellipse(s4x,s1y,ewid,ehei,0,0,2*Math.PI);
+            ctx.fillStyle = '#58a64c';
+            ctx.fill();
+ 
+ 
+        }
+
+        // runs when DOM window resize fires
+        function resizeCanvas() {
+            var mynav = document.getElementById("base-nav");
+            logoCanvas.height = mynav.offsetHeight;
+            logoCanvas.width = (logoCanvas.height)*4;
+            redraw();
+        }
+    } else { 
+        // canvas unsupported code here
     }
-}
+
+});
 
 // toggle nav menu
 function toggleNavMenu() {
